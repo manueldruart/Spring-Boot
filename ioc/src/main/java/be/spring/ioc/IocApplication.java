df2841a93797bb4b3104c544721fac0d138b1a9e
manueldruart <manuel.druart@gmail.com>
2021-12-08 13:01:35 +0100

    test

---
+++ ioc/src/main/java/be/spring/ioc/IocApplication.java
@@ -0,0 +1,14 @@
+package be.spring.ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// my first project
+@SpringBootApplication
+public class IocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocApplication.class, args);
+	}
+
+}
+
---
+++ ioc/src/test/java/be/spring/ioc/IocApplicationTests.java
@@ -0,0 +1,13 @@
+package be.spring.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
